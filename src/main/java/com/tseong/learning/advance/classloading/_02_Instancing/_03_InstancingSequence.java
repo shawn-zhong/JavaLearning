af7f73c6288a26d3525bec40218a75b642bd6884
Shawn Zhong <tseong@foxmail.com>
2018-02-25 15:11:32 +0800

    Instancing sequence

--- src/main/java/com/tseong/learning/advance/classloading/_02_Instancing/_03_InstancingSequence.java
+++ src/main/java/com/tseong/learning/advance/classloading/_02_Instancing/_03_InstancingSequence.java
@@ -1,4 +1,86 @@
 package com.tseong.learning.advance.classloading._02_Instancing;
 
-public class _03_InstancingSequence {
+import com.sun.xml.internal.rngom.parse.host.Base;
+
+class BaseClass {
+    int i = 1;
+
+    // running sequence : step-4
+    BaseClass() {
+        System.out.println("BaseClass Constructor: i = " + i);          // 经过step-3, 此时i=2
+        int x = getValue();                                             // 调用的是子类重载的getValue方法，此时j还没有初始化，所以返回0
+        System.out.println("BaseClass Constructor: getValue() = " + x);
+    }
+
+    // running sequence : step-3
+    {
+        i = 2;
+        System.out.println("BaseClass non-static block");
+    }
+
+    // running sequence : step-1
+    static {
+        System.out.println("BaseClass static block");
+    }
+
+    protected int getValue() {
+
+        System.out.println("BaseClass getValue() , i = " + i);
+        return i;
+    }
 }
+
+class SubClass extends BaseClass {
+    int j = 1;
+
+    // running sequence : step-6
+    SubClass () {
+        System.out.println("SubClass Constructor: j = " + j);                       // 经过step-6, 此时j的值为3
+        System.out.println("SubClass Constructor: getValue() = " + getValue());     // 经过step-6, 此时j的值为3
+    }
+
+    // running sequence : step-5
+    {
+        j = 3;
+        System.out.println("SubClass non-static block");
+    }
+
+    // running sequence : step-2
+    static {
+        System.out.println("SubClass static block");
+    }
+
+    @Override
+    protected int getValue() {
+        System.out.println("SubClass getValue() , i=" + i + " j=" + j);
+        return j;
+    }
+}
+
+
+public class _03_InstancingSequence {
+
+    public static void main(String[] args) {
+        SubClass instance = new SubClass();
+        System.out.println("main : " + instance.getValue());    // running sequence : step-7
+    }
+}
+
+
+// 输出:
+/*
+
+BaseClass static block
+SubClass static block
+BaseClass non-static block
+BaseClass Constructor: i = 2
+SubClass getValue() , i=2 j=0
+BaseClass Constructor: getValue() = 0
+SubClass non-static block
+SubClass Constructor: j = 3
+SubClass getValue() , i=2 j=3
+SubClass Constructor: getValue() = 3
+SubClass getValue() , i=2 j=3
+main : 3
+
+ */
---
+++ src/main/java/com/tseong/learning/advance/classloading/_02_Instancing/_04_InstancingSequence.java
@@ -0,0 +1,51 @@
+package com.tseong.learning.advance.classloading._02_Instancing;
+
+// 这个例子关键在于说明类的初始化操作和类的实例化操作可以是并行的，也就是类的实例化操作可以在初始化操作完成之前完成
+public class _04_InstancingSequence {
+
+    public static void main(String[] args) {
+        staticFunction();
+    }
+
+    // 类初始化执行顺序－1 ：这是第一句赋值语句，“在类都没有初始化完毕之前，能直接进行实例化相应的对象吗？”
+    // 实例初始化嵌入到了静态初始化流程中，并且在上面的程序中，嵌入到了静态初始化的起始位置。这就导致了实例初始化完全发生在静态初始化之前
+    // 也就是说执行完这一句会打印：
+    // non-static block
+    // In Constructor
+    // a=110 b=0  // -> 因为b是静态变量，类只完成实例化，没有完成初始化，所以b的值为0
+    static _04_InstancingSequence st = new _04_InstancingSequence();
+
+    // 类初始化执行顺序－2
+    static {
+        System.out.println("static block");
+    }
+
+    // 类的实例化执行顺序-1
+    {
+        System.out.println("non-static block");
+    }
+
+    // 类的实例化执行顺序-2
+    public _04_InstancingSequence() {
+        System.out.println("In Constructor");
+        System.out.println("a=" + a + " b=" + b);
+    }
+
+    //
+    public static void staticFunction() {
+        System.out.println("In staticFunction(); b=" +b);
+    }
+
+    int a = 110;
+
+    // 类初始化执行顺序－3
+    static int b = 112;
+
+    // 类初始化执行顺序－4
+    // 此时b已经完成类初始化，已经有值－2
+    // 执行到这一句，打印：
+    // non-static block
+    // In Constructor
+    // a=110 b=112
+    static _04_InstancingSequence st2 = new _04_InstancingSequence();
+}
