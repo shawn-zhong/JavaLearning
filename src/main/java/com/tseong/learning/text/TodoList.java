da6fec3262603f0d4e6a43e18f6b666119e7b0e8
Shawn Zhong <tseong@foxmail.com>
2018-04-12 16:36:36 +0800

    changes

---
+++ src/main/java/com/tseong/learning/text/TodoList.java
@@ -0,0 +1,16 @@
+package com.tseong.learning.text;
+
+public class TodoList {
+
+    /*
+
+    1. Maven配置文件与Nexus的使用
+    2. @Component 和 @Bean 的使用，构建项目
+    3. MyBatis注解的使用
+    4. Rabbit MQ 的使用
+    5. Dubbo和Dubbox的使用
+    6. zookeeper的使用
+    7. Docker的使用
+
+     */
+}
