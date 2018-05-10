59c3b6d24be23c453feecf26994a41a76e2c6b48
Shawn Zhong <tseong@foxmail.com>
2018-05-10 15:18:57 +0800

    patterns

---
+++ src/main/java/com/tseong/learning/patterns/_18_command/Order.java
@@ -0,0 +1,5 @@
+package com.tseong.learning.patterns._18_command;
+
+public interface Order {
+    void execute();
+}
---
+++ src/main/java/com/tseong/learning/patterns/_18_command/Stock.java
@@ -0,0 +1,15 @@
+package com.tseong.learning.patterns._18_command;
+
+public class Stock {
+
+    private String name = "ABC";
+    private int quantity = 10;
+
+    public void buy() {
+        System.out.println("Stock [ Name: " + name + ", Quantity: " + quantity + " ] bought");
+    }
+
+    public void sell() {
+        System.out.println("Stock [ Name: " + name + ", Quantity: " + quantity + " ] sold");
+    }
+}
