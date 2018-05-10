408063af5d2301316bd12e26a07b0f545dc9f2fa
Shawn Zhong <tseong@foxmail.com>
2018-05-10 10:53:03 +0800

    patterns

--- src/main/java/com/tseong/learning/patterns/_12_composite/Employee.java
+++ src/main/java/com/tseong/learning/patterns/_12_composite/Employee.java
@@ -1,4 +1,36 @@
 package com.tseong.learning.patterns._12_composite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee {
+
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    // constructor
+    public Employee(String name, String dept, int sal) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = sal;
+        subordinates = new ArrayList<>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public String toString() {
+        return ("Employee :[ Name : " + name + ", dept : " + dept + ", salary :" + salary+" ]");
+    }
 }
