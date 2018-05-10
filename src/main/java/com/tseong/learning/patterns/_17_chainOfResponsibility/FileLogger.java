141e9ea6559d4891a1aee8e2041fee6f83e4ecea
Shawn Zhong <tseong@foxmail.com>
2018-05-10 14:44:58 +0800

    patterns

---
+++ src/main/java/com/tseong/learning/patterns/_17_chainOfResponsibility/FileLogger.java
@@ -0,0 +1,13 @@
+package com.tseong.learning.patterns._17_chainOfResponsibility;
+
+public class FileLogger extends AbstractLogger {
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
