7bf98b34ae85fb44f156c87494d18a4ff884c064
Shawn Zhong <tseong@foxmail.com>
2018-05-11 11:41:00 +0800

    design patterns

---
+++ src/main/java/com/tseong/learning/patterns/_26_mvc/MVCPatternDemo.java
@@ -0,0 +1,27 @@
+package com.tseong.learning.patterns._26_mvc;
+
+public class MVCPatternDemo {
+
+    public static void main(String[] args) {
+
+        // fetch student record based on this roll no from database
+        Student model = retriveStudentFromDatabase();
+
+        // create a view : to write student details on console
+        StudentView view = new StudentView();
+
+        StudentController controller = new StudentController(model, view);
+        controller.updateView();
+
+        // update model data
+        controller.setStudentName("Shawn");
+        controller.updateView();
+    }
+
+    private static Student retriveStudentFromDatabase() {
+        Student student = new Student();
+        student.setName("Robert");
+        student.setRollNo("10");
+        return student;
+    }
+}
